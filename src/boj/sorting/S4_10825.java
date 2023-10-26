ff2ccbc68090fd0228b545133600e42493cb051b
Junsun Lee <junsun1014@naver.com>
2023-10-26 23:16:50 +0900

    23.10.26

--- src/boj/sorting/S4_10825.java
+++ src/boj/sorting/S4_10825.java
@@ -8,14 +8,14 @@ import java.io.*;
 import java.util.*;
 
 public class S4_10825 {
-    public static int n;
-    public static ArrayList<Student> students = new ArrayList<>();
+    public static int n;    // 학생 수
+    public static ArrayList<Student> students = new ArrayList<>();  // 학생 배열
 
     static class Student implements Comparable<Student> {
-        String name;
-        int korean;
-        int english;
-        int math;
+        String name;    // 이름
+        int korean;     // 국어 점수
+        int english;    // 영어 점수
+        int math;       // 수학 점수
 
         Student(String name, int korean, int english, int math) {
             this.name = name;
@@ -30,19 +30,19 @@ public class S4_10825 {
                 if (this.english == x.english) {
                     if (this.math == x.math) {
                         for (int i = 0; i < Math.min(this.name.length(), x.name.length()); i++) {
+                            // 사전순
                             if (this.name.charAt(i) > x.name.charAt(i)) {
                                 return 1;
                             } else if (this.name.charAt(i) < x.name.charAt(i)) {
                                 return -1;
                             }
                         }
-
                     }
-                    return x.math - this.math;
+                    return x.math - this.math;  // 내림차순
                 }
-                return this.english - x.english;
+                return this.english - x.english;    // 오름차순
             }
-            return x.korean - this.korean;
+            return x.korean - this.korean;  // 내림차순
         }
     }
 
@@ -53,7 +53,7 @@ public class S4_10825 {
 
     public static void read() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());    // 학생 수 입력
         StringTokenizer st;
 
         for (int i = 0; i < n; i++) {
@@ -63,12 +63,12 @@ public class S4_10825 {
             int english = Integer.parseInt(st.nextToken());
             int math = Integer.parseInt(st.nextToken());
             Student student = new Student(name,korean,english,math);
-            students.add(student);
+            students.add(student);  // 학생 추가
         }
     }
 
     public static void solution() {
-        Collections.sort(students);
+        Collections.sort(students); // 정렬
         for (int i = 0; i < n; i++) {
             System.out.println(students.get(i).name);
         }
